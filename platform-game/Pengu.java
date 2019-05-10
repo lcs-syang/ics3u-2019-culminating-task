672cf2f1edf5747d02478cd910e6e6117b1ca787
Selena Yang <syang@lcs.on.ca>
2019-05-10 13:44:33 -0400

    Tutorial in progress

--- platform-game/Pengu.java
+++ platform-game/Pengu.java
@@ -9,7 +9,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Pengu extends Actor
 {
     private int speed = 7;
-    
+    private int vSpeed = 0;
+    private int acceleration = 2; 
+    private int jumpStrength = 13;
     /**
      * Act - do whatever the Pengu wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -17,6 +19,7 @@ public class Pengu extends Actor
     public void act() 
     {
         checkKeys();
+        checkFall();
     }
 
     private void checkKeys()
@@ -29,8 +32,36 @@ public class Pengu extends Actor
             setImage("pengu-right.png");
             moveRight();
         }
+        if (Greenfoot.isKeyDown("space")) 
+        {
+           jump(); 
+        }
     }
-
+    
+    public void jump()
+    {
+        vSpeed = -jumpStrength;
+        fall();
+    }
+    public void checkFall()
+    {
+        if(onGround()){
+            vSpeed = 0;
+        }
+        else {
+            fall();
+        }
+    }
+    public boolean onGround()
+    {
+        Actor under = getOneObjectAtOffset (0, getImage().getHeight()/2, Ground.class);
+        return under != null; 
+    }
+    public void fall()
+    {
+        setLocation(getX(), getY() + vSpeed);
+        vSpeed = vSpeed + acceleration;
+    }
     public void moveRight()
     {
         setLocation(getX() + speed, getY());
